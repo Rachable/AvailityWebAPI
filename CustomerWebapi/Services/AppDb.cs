31f3f8ec91d9a0510a4649e19cb8e26ef8fd99a4
Rachable <rachel.e.etter@gmail.com>
2020-10-11 12:20:19 -0400

    Added Exception Handling

---
+++ CustomerWebapi/Services/AppDb.cs
@@ -0,0 +1,18 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using MySql.Data.MySqlClient;
+
+namespace Services
+{
+    public class AppDb : IDisposable
+    {
+        public MySqlConnection Connection { get; }
+
+        public AppDb(string connectionString)
+        {
+            Connection = new MySqlConnection(connectionString);
+        }
+
+        public void Dispose() => Connection.Dispose();
+    }
+}

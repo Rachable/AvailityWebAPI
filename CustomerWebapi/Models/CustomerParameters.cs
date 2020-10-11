21e9d39532e3de064bfa6c471f3b68a635a03de7
Rachable <rachel.e.etter@gmail.com>
2020-10-11 18:30:38 -0400

    Added Pagination and Formatting

---
+++ CustomerWebapi/Models/CustomerParameters.cs
@@ -0,0 +1,22 @@
+
+namespace Models
+{
+    public class CustomerParameters
+    {
+        const int maxPageSize = 50;
+        public int PageNumber { get; set; } = 1;
+
+        private int _pageSize = 10;
+        public int PageSize
+        {
+            get
+            {
+                return _pageSize;
+            }
+            set
+            {
+                _pageSize = (value > maxPageSize) ? maxPageSize : value;
+            }
+        }
+    }
+}
